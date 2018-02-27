559e20522b5cbd746a8d53e41e7df3b8c5822f1c
Michael Roy <michael@michaels-macbook-pro.local>
2018-02-26 22:54:10 -0700

    Basic UI

--- ChordTransposer/ViewController.swift
+++ ChordTransposer/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sharpsOrFlatsLabel: UILabel!
+    @IBOutlet weak var sharpsOrFlatsSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
+    @IBAction func sharpsOrFlatsToggled(_ sender: UISwitch) {
+        if (sender.isOn) {
+            sharpsOrFlatsLabel.text = "Sharp Keys"
+        }
+        else {
+            sharpsOrFlatsLabel.text = "Flat Keys"
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
